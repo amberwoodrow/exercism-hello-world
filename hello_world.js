a281363a9d3341d5d99f3f611cae9cea89da2eff
amberwoodrow <amberwoodrow@gmail.com>
2015-11-14 13:53:24 -0700

    initial commit

---
+++ hello_world.js
@@ -0,0 +1,10 @@
+var HelloWorld = function() {};
+
+HelloWorld.prototype.hello = function(input) {
+  if (input) {
+    return "Hello, "+input+"!";
+  }
+  return "Hello, World!";
+};
+
+module.exports = HelloWorld;
